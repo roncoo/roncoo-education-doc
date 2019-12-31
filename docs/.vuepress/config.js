6474a13f3640e1f12c294f2c7bde334c05b15e97
fengyongwei <fengyw@roncoo.com>
2019-12-31 10:40:32 +0800

    初始化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,12 @@
     module.exports = {
     port: 80,
     title: '龙果博客 - 领课教育',
-	base: '/roncoo-education/',
     themeConfig: {
         head: [
-            ['link', { rel: 'icon', href: `/favicon.ico` }]
+            ['link', { rel: 'icon', href: '/favicon.ico' }]
         ],
         nav: [
-            {text: '指南', link: '/guide/'}
+            {text: '指南', link: '/roncoo-education/'}
         ],
         sidebar: 'auto'
     }
