7ad033a7c5f7dfdfbed32c0ab71a279810b78fef
fengyongwei <fengyw@roncoo.com>
2020-01-04 14:32:56 +0800

    初始化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,10 @@
             ['link', { rel: 'icon', href: '/favicon.ico' }]
         ],
         nav: [
-            {text: '指南', link: '/roncoo-education/'}
+            {text: '指南', link: '/roncoo-education/'},
+            { text: '商业版', link: 'https://edu.roncoo.net/business.html'},
+            { text: 'GitHub', link: 'https://github.com/roncoo/roncoo-education'},
+            { text: 'Gitee', link: 'https://gitee.com/roncoocom/roncoo-education'}
         ],
         sidebar: 'auto'
     }
